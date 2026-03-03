4fe8b621ebe9c1275c6aa7ce3ec02dae30852a2a
Chetan Garg <chetangarg21154@gmail.com>
2026-03-03 14:17:06 +0530

    Updated server for Render deployment

--- Exp5.3/index.js
+++ Exp5.3/index.js
@@ -88,6 +88,8 @@ app.get("/category-ratings", async (req, res) => {
 });
 
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
